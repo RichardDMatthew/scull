e7ec218bad34b10c14cd30ed100a1a2455a016eb
Rick Matthew <richard.d.matthew@gmail.com>
2021-06-23 00:15:52 -0700

    mostly cleaned up now, have problem with copy_to_user and copy_from_user

--- main.c
+++ main.c
@@ -41,14 +41,10 @@
 int swaphints_major =   SWAPHINTS_MAJOR;
 int swaphints_minor =   0;
 int swaphints_nr_devs = SWAPHINTS_NR_DEVS;	/* number of bare swaphints devices */
-//int swaphints_quantum = SWAPHINTS_QUANTUM;
-//int swaphints_qset =    SWAPHINTS_QSET;
 
 module_param(swaphints_major, int, S_IRUGO);
 module_param(swaphints_minor, int, S_IRUGO);
 module_param(swaphints_nr_devs, int, S_IRUGO);
-//module_param(swaphints_quantum, int, S_IRUGO);
-//module_param(swaphints_qset, int, S_IRUGO);
 
 MODULE_AUTHOR("Alessandro Rubini, Jonathan Corbet");
 MODULE_LICENSE("Dual BSD/GPL");
@@ -56,183 +52,6 @@ MODULE_LICENSE("Dual BSD/GPL");
 struct swaphints_dev *swaphints_devices;	/* allocated in swaphints_init_module */
 swaphints_request_t *requests;
 swaphints_response_t *responses;
-
-/*
- * Empty out the swaphints device; must be called with the device
- * semaphore held.
- */
-// int swaphints_trim(struct swaphints_dev *dev)
-// {
-// 	struct swaphints_qset *next, *dptr;
-// 	int qset = dev->qset;   /* "dev" is not-null */
-// 	int i;
-
-// 	for (dptr = dev->data; dptr; dptr = next) { /* all the list items */
-// 		if (dptr->data) {
-// 			for (i = 0; i < qset; i++)
-// 				kfree(dptr->data[i]);
-// 			kfree(dptr->data);
-// 			dptr->data = NULL;
-// 		}
-// 		next = dptr->next;
-// 		kfree(dptr);
-// 	}
-// 	dev->size = 0;
-// 	dev->quantum = swaphints_quantum;
-// 	dev->qset = swaphints_qset;
-// 	dev->data = NULL;
-// 	return 0;
-//}
-// #ifdef SWAPHINTS_DEBUG /* use proc only if debugging */
-// /*
-//  * The proc filesystem: function to read and entry
-//  */
-
-// int swaphints_read_procmem(struct seq_file *s, void *v)
-// {
-//         int i, j;
-//         int limit = s->size - 80; /* Don't print more than this */
-
-//         for (i = 0; i < swaphints_nr_devs && s->count <= limit; i++) {
-//                 struct swaphints_dev *d = &swaphints_devices[i];
-//                 struct swaphints_qset *qs = d->data;
-//                 if (mutex_lock_interruptible(&d->lock))
-//                         return -ERESTARTSYS;
-//                 seq_printf(s,"\nDevice %i: qset %i, q %i, sz %li\n",
-//                              i, d->qset, d->quantum, d->size);
-//                 for (; qs && s->count <= limit; qs = qs->next) { /* scan the list */
-//                         seq_printf(s, "  item at %p, qset at %p\n",
-//                                      qs, qs->data);
-//                         if (qs->data && !qs->next) /* dump only the last item */
-//                                 for (j = 0; j < d->qset; j++) {
-//                                         if (qs->data[j])
-//                                                 seq_printf(s, "    % 4i: %8p\n",
-//                                                              j, qs->data[j]);
-//                                 }
-//                 }
-//                 mutex_unlock(&swaphints_devices[i].lock);
-//         }
-//         return 0;
-// }
-
-
-
-// /*
-//  * Here are our sequence iteration methods.  Our "position" is
-//  * simply the device number.
-//  */
-// static void *swaphints_seq_start(struct seq_file *s, loff_t *pos)
-// {
-// 	if (*pos >= swaphints_nr_devs)
-// 		return NULL;   /* No more to read */
-// 	return swaphints_devices + *pos;
-// }
-
-// static void *swaphints_seq_next(struct seq_file *s, void *v, loff_t *pos)
-// {
-// 	(*pos)++;
-// 	if (*pos >= swaphints_nr_devs)
-// 		return NULL;
-// 	return swaphints_devices + *pos;
-// }
-
-// static void swaphints_seq_stop(struct seq_file *s, void *v)
-// {
-// 	/* Actually, there's nothing to do here */
-// }
-
-// static int swaphints_seq_show(struct seq_file *s, void *v)
-// {
-// 	struct swaphints_dev *dev = (struct swaphints_dev *) v;
-// 	struct swaphints_qset *d;
-// 	int i;
-
-// 	if (mutex_lock_interruptible(&dev->lock))
-// 		return -ERESTARTSYS;
-// 	seq_printf(s, "\nDevice %i: qset %i, q %i, sz %li\n",
-// 			(int) (dev - swaphints_devices), dev->qset,
-// 			dev->quantum, dev->size);
-// 	for (d = dev->data; d; d = d->next) { /* scan the list */
-// 		seq_printf(s, "  item at %p, qset at %p\n", d, d->data);
-// 		if (d->data && !d->next) /* dump only the last item */
-// 			for (i = 0; i < dev->qset; i++) {
-// 				if (d->data[i])
-// 					seq_printf(s, "    % 4i: %8p\n",
-// 							i, d->data[i]);
-// 			}
-// 	}
-// 	mutex_unlock(&dev->lock);
-// 	return 0;
-// }
-	
-// /*
-//  * Tie the sequence operators up.
-//  */
-// static struct seq_operations swaphints_seq_ops = {
-// 	.start = swaphints_seq_start,
-// 	.next  = swaphints_seq_next,
-// 	.stop  = swaphints_seq_stop,
-// 	.show  = swaphints_seq_show
-// };
-
-// /*
-//  * Now to implement the /proc files we need only make an open
-//  * method which sets up the sequence operators.
-//  */
-// static int swaphintsmem_proc_open(struct inode *inode, struct file *file)
-// {
-// 	return single_open(file, swaphints_read_procmem, NULL);
-// }
-
-// static int swaphintsseq_proc_open(struct inode *inode, struct file *file)
-// {
-// 	return seq_open(file, &swaphints_seq_ops);
-// }
-
-// /*
-//  * Create a set of file operations for our proc files.
-//  */
-// static struct file_operations swaphintsmem_proc_ops = {
-// 	.owner   = THIS_MODULE,
-// 	.open    = swaphintsmem_proc_open,
-// 	.read    = seq_read,
-// 	.llseek  = seq_lseek,
-// 	.release = single_release
-// };
-
-// static struct file_operations swaphintsseq_proc_ops = {
-// 	.owner   = THIS_MODULE,
-// 	.open    = swaphintsseq_proc_open,
-// 	.read    = seq_read,
-// 	.llseek  = seq_lseek,
-// 	.release = seq_release
-// };
-	
-
-// /*
-//  * Actually create (and remove) the /proc file(s).
-//  */
-
-// static void swaphints_create_proc(void)
-// {
-// 	proc_create_data("swaphintsmem", 0 /* default mode */,
-// 			NULL /* parent dir */, proc_ops_wrapper(&swaphintsmem_proc_ops, swaphintsmem_pops),
-// 			NULL /* client data */);
-// 	proc_create("swaphintsseq", 0, NULL, proc_ops_wrapper(&swaphintsseq_proc_ops, swaphintsseq_pops));
-// }
-
-// static void swaphints_remove_proc(void)
-// {
-// 	/* no problem if it was not registered */
-// 	remove_proc_entry("swaphintsmem", NULL /* parent dir */);
-// 	remove_proc_entry("swaphintsseq", NULL);
-// }
-
-
-// #endif /* SWAPHINTS_DEBUG */
-
-
-
 
 
 /*
@@ -246,13 +65,6 @@ int swaphints_open(struct inode *inode, struct file *filp)
 	dev = container_of(inode->i_cdev, struct swaphints_dev, cdev);
 	filp->private_data = dev; /* for other methods */
 
-	/* now trim to 0 the length of the device if open was write-only */
-	// if ( (filp->f_flags & O_ACCMODE) == O_WRONLY) {
-	// 	// if (mutex_lock_interruptible(&dev->lock))
-	// 	// 	return -ERESTARTSYS;
-	// 	//swaphints_trim(dev); /* ignore errors */
-	// 	//mutex_unlock(&dev->lock);
-	// }
 	return 0;          /* success */
 }
 
@@ -260,135 +72,6 @@ int swaphints_release(struct inode *inode, struct file *filp)
 {
 	return 0;
 }
-/*
- * Follow the list
- */
-// struct swaphints_qset *swaphints_follow(struct swaphints_dev *dev, int n)
-// {
-// 	struct swaphints_qset *qs = dev->data;
-
-//         /* Allocate first qset explicitly if need be */
-// 	if (! qs) {
-// 		qs = dev->data = kmalloc(sizeof(struct swaphints_qset), GFP_KERNEL);
-// 		if (qs == NULL)
-// 			return NULL;  /* Never mind */
-// 		memset(qs, 0, sizeof(struct swaphints_qset));
-// 	}
-
-// 	/* Then follow the list */
-// 	while (n--) {
-// 		if (!qs->next) {
-// 			qs->next = kmalloc(sizeof(struct swaphints_qset), GFP_KERNEL);
-// 			if (qs->next == NULL)
-// 				return NULL;  /* Never mind */
-// 			memset(qs->next, 0, sizeof(struct swaphints_qset));
-// 		}
-// 		qs = qs->next;
-// 		continue;
-// 	}
-// 	return qs;
-// }
-
-/*
- * Data management: read and write
- */
-
-// ssize_t swaphints_read(struct file *filp, char __user *buf, size_t count,
-//                 loff_t *f_pos)
-// {
-// 	struct swaphints_dev *dev = filp->private_data; 
-// 	struct swaphints_qset *dptr;	/* the first listitem */
-// 	int quantum = dev->quantum, qset = dev->qset;
-// 	int itemsize = quantum * qset; /* how many bytes in the listitem */
-// 	int item, s_pos, q_pos, rest;
-// 	ssize_t retval = 0;
-
-// 	if (mutex_lock_interruptible(&dev->lock))
-// 		return -ERESTARTSYS;
-// 	if (*f_pos >= dev->size)
-// 		goto out;
-// 	if (*f_pos + count > dev->size)
-// 		count = dev->size - *f_pos;
-
-// 	/* find listitem, qset index, and offset in the quantum */
-// 	item = (long)*f_pos / itemsize;
-// 	rest = (long)*f_pos % itemsize;
-// 	s_pos = rest / quantum; q_pos = rest % quantum;
-
-// 	/* follow the list up to the right position (defined elsewhere) */
-// 	dptr = swaphints_follow(dev, item);
-
-// 	if (dptr == NULL || !dptr->data || ! dptr->data[s_pos])
-// 		goto out; /* don't fill holes */
-
-// 	/* read only up to the end of this quantum */
-// 	if (count > quantum - q_pos)
-// 		count = quantum - q_pos;
-
-// 	if (copy_to_user(buf, dptr->data[s_pos] + q_pos, count)) {
-// 		retval = -EFAULT;
-// 		goto out;
-// 	}
-// 	*f_pos += count;
-// 	retval = count;
-
-//   out:
-// 	mutex_unlock(&dev->lock);
-// 	return retval;
-// }
-
-// ssize_t swaphints_write(struct file *filp, const char __user *buf, size_t count,
-//                 loff_t *f_pos)
-// {
-// 	struct swaphints_dev *dev = filp->private_data;
-// 	struct swaphints_qset *dptr;
-// 	int quantum = dev->quantum, qset = dev->qset;
-// 	int itemsize = quantum * qset;
-// 	int item, s_pos, q_pos, rest;
-// 	ssize_t retval = -ENOMEM; /* value used in "goto out" statements */
-
-// 	if (mutex_lock_interruptible(&dev->lock))
-// 		return -ERESTARTSYS;
-
-// 	/* find listitem, qset index and offset in the quantum */
-// 	item = (long)*f_pos / itemsize;
-// 	rest = (long)*f_pos % itemsize;
-// 	s_pos = rest / quantum; q_pos = rest % quantum;
-
-// 	/* follow the list up to the right position */
-// 	dptr = swaphints_follow(dev, item);
-// 	if (dptr == NULL)
-// 		goto out;
-// 	if (!dptr->data) {
-// 		dptr->data = kmalloc(qset * sizeof(char *), GFP_KERNEL);
-// 		if (!dptr->data)
-// 			goto out;
-// 		memset(dptr->data, 0, qset * sizeof(char *));
-// 	}
-// 	if (!dptr->data[s_pos]) {
-// 		dptr->data[s_pos] = kmalloc(quantum, GFP_KERNEL);
-// 		if (!dptr->data[s_pos])
-// 			goto out;
-// 	}
-// 	/* write only up to the end of this quantum */
-// 	if (count > quantum - q_pos)
-// 		count = quantum - q_pos;
-
-// 	if (copy_from_user(dptr->data[s_pos]+q_pos, buf, count)) {
-// 		retval = -EFAULT;
-// 		goto out;
-// 	}
-// 	*f_pos += count;
-// 	retval = count;
-
-//         /* update the size */
-// 	if (dev->size < *f_pos)
-// 		dev->size = *f_pos;
-
-//   out:
-// 	mutex_unlock(&dev->lock);
-// 	return retval;
-// }
 
 /*
  * The ioctl() implementation
@@ -420,102 +103,20 @@ long swaphints_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	if (err) return -EFAULT;
 
 	switch(cmd) {
-                
-	//   case SWAPHINTS_IOCSQSET:
-	// 	if (! capable (CAP_SYS_ADMIN))
-	// 		return -EPERM;
-	// 	retval = __get_user(swaphints_qset, (int __user *)arg);
-	// 	break;
-
-	//   case SWAPHINTS_IOCTQSET:
-	// 	if (! capable (CAP_SYS_ADMIN))
-	// 		return -EPERM;
-	// 	swaphints_qset = arg;
-	// 	break;
-
-	//   case SWAPHINTS_IOCGQSET:
-	// 	retval = __put_user(swaphints_qset, (int __user *)arg);
-	// 	break;
-
-	//   case SWAPHINTS_IOCQQSET:
-	// 	return swaphints_qset;
-
-	//   case SWAPHINTS_IOCXQSET:
-	// 	if (! capable (CAP_SYS_ADMIN))
-	// 		return -EPERM;
-	// 	tmp = swaphints_qset;
-	// 	retval = __get_user(swaphints_qset, (int __user *)arg);
-	// 	if (retval == 0)
-	// 		retval = put_user(tmp, (int __user *)arg);
-	// 	break;
-
-	//   case SWAPHINTS_IOCHQSET:
-	// 	if (! capable (CAP_SYS_ADMIN))
-	// 		return -EPERM;
-	// 	tmp = swaphints_qset;
-	// 	swaphints_qset = arg;
-	// 	return tmp;
-
-        /*
-         * The following two change the buffer size for swaphintspipe.
-         * The swaphintspipe device uses this same ioctl method, just to
-         * write less code. Actually, it's the same driver, isn't it?
-         */
-
-	//   case SWAPHINTS_P_IOCTSIZE:
-	// 	swaphints_p_buffer = arg;
-	// 	break;
-
-	//   case SWAPHINTS_P_IOCQSIZE:
-	// 	return swaphints_p_buffer;
-
-
-	  default:  /* redundant, as cmd was checked against MAXNR */
-		return -ENOTTY;
+		case SWAPHINTS_SEND_REQUEST:
+			copy_from_user(&requests, (char *)arg, sizeof(struct swaphints_request));
+			break;
+		case SWAPHINTS_GET_RESPONSE:
+			copy_to_user((char *)arg, &responses, sizeof(swaphints_request_t));
+			break;
+	  	default:  /* redundant, as cmd was checked against MAXNR */
+			return -ENOTTY;
 	}
 	return retval;
-
 }
-
-
-
-/*
- * The "extended" operations -- only seek
- */
-
-// loff_t swaphints_llseek(struct file *filp, loff_t off, int whence)
-// {
-// 	struct swaphints_dev *dev = filp->private_data;
-// 	loff_t newpos;
-
-// 	switch(whence) {
-// 	  case 0: /* SEEK_SET */
-// 		newpos = off;
-// 		break;
-
-// 	  case 1: /* SEEK_CUR */
-// 		newpos = filp->f_pos + off;
-// 		break;
-
-// 	  case 2: /* SEEK_END */
-// 		newpos = dev->size + off;
-// 		break;
-
-// 	  default: /* can't happen */
-// 		return -EINVAL;
-// 	}
-// 	if (newpos < 0) return -EINVAL;
-// 	filp->f_pos = newpos;
-// 	return newpos;
-// }
-
-
 
 struct file_operations swaphints_fops = {
 	.owner =    THIS_MODULE,
-	//.llseek =   swaphints_llseek,
-	//.read =     swaphints_read,
-	//.write =    swaphints_write,
 	.unlocked_ioctl = swaphints_ioctl,
 	.open =     swaphints_open,
 	.release =  swaphints_release,
@@ -538,22 +139,13 @@ void swaphints_cleanup_module(void)
 	/* Get rid of our char dev entries */
 	if (swaphints_devices) {
 		for (i = 0; i < swaphints_nr_devs; i++) {
-			//swaphints_trim(swaphints_devices + i);
 			cdev_del(&swaphints_devices[i].cdev);
 		}
 		kfree(swaphints_devices);
 	}
 
-// #ifdef SWAPHINTS_DEBUG /* use proc only if debugging */
-// 	swaphints_remove_proc();
-// #endif
-
 	/* cleanup_module is never called if registering failed */
 	unregister_chrdev_region(devno, swaphints_nr_devs);
-
-	/* and call the cleanup functions for friend devices */
-	//swaphints_p_cleanup();
-	//swaphints_access_cleanup();
 
 }
 
@@ -609,20 +201,11 @@ int swaphints_init_module(void)
 
         /* Initialize each device. */
 	for (i = 0; i < swaphints_nr_devs; i++) {
-		//swaphints_devices[i].quantum = swaphints_quantum;
-		//swaphints_devices[i].qset = swaphints_qset;
-		//mutex_init(&swaphints_devices[i].lock);
 		swaphints_setup_cdev(&swaphints_devices[i], i);
 	}
 
         /* At this point call the init function for any friend device */
 	dev = MKDEV(swaphints_major, swaphints_minor + swaphints_nr_devs);
-	//dev += swaphints_p_init(dev);
-	//dev += swaphints_access_init(dev);
-
-// #ifdef SWAPHINTS_DEBUG /* only when debugging */
-// 	swaphints_create_proc();
-// #endif
 
 	return 0; /* succeed */
 
--- swaphints.h
+++ swaphints.h
@@ -19,26 +19,7 @@
 #define _SWAPHINTS_H_
 
 #include <linux/ioctl.h> /* needed for the _IOW etc stuff used later */
-
-/*
- * Macros to help debugging
- */
-
-//#undef PDEBUG             /* undef it, just in case */
-// #ifdef SWAPHINTS_DEBUG
-// #  ifdef __KERNEL__
-//      /* This one if debugging is on, and kernel space */
-// #    define PDEBUG(fmt, args...) printk( KERN_DEBUG "swaphints: " fmt, ## args)
-// #  else
-//      /* This one for user space */
-// #    define PDEBUG(fmt, args...) fprintf(stderr, fmt, ## args)
-// #  endif
-// #else
-// #  define PDEBUG(fmt, args...) /* not debugging: nothing */
-// #endif
-
-// #undef PDEBUGG
-// #define PDEBUGG(fmt, args...) /* nothing: it's a placeholder */
+#include <linux/types.h> /* needed for a common definition of uints */
 
 #ifndef SWAPHINTS_MAJOR
 #define SWAPHINTS_MAJOR 0   /* dynamic major by default */
@@ -51,38 +32,6 @@
 #ifndef SWAPHINTS_P_NR_DEVS
 #define SWAPHINTS_P_NR_DEVS 1  /* swaphintspipe0 through swaphintspipe3 */
 #endif
-
-/*
- * The bare device is a variable-length region of memory.
- * Use a linked list of indirect blocks.
- *
- * "swaphints_dev->data" points to an array of pointers, each
- * pointer refers to a memory area of SWAPHINTS_QUANTUM bytes.
- *
- * The array (quantum-set) is SWAPHINTS_QSET long.
- */
-// #ifndef SWAPHINTS_QUANTUM
-// #define SWAPHINTS_QUANTUM 4000
-// #endif
-
-// #ifndef SWAPHINTS_QSET
-// #define SWAPHINTS_QSET    1000
-// #endif
-
-/*
- * The pipe device is a simple circular buffer. Here its default size
- */
-// #ifndef SWAPHINTS_P_BUFFER
-// #define SWAPHINTS_P_BUFFER 4000
-// #endif
-
-/*
- * Representation of swaphints quantum sets.
- */
-// struct swaphints_qset {
-// 	void **data;
-// 	struct swaphints_qset *next;
-// };
 
 /* start of swaphints structs */
 #define MAX_PFNCOUNT 10
@@ -103,12 +52,6 @@ typedef struct swaphints_response{
  */
 
 struct swaphints_dev {
-	//struct swaphints_qset *data;  /* Pointer to first quantum set */
-	//int quantum;              /* the current quantum size */
-	//int qset;                 /* the current array size */
-	//unsigned long size;       /* amount of data stored here */
-	//unsigned int access_key;  /* used by swaphintsuid and swaphintspriv */
-	//struct mutex lock;     /* mutual exclusion semaphore     */
 	swaphints_request_t requests;	/* requests being sent down */
 	swaphints_response_t responses;	/* responses being returned */
 	struct cdev cdev;	  /* Char device structure		*/
@@ -120,36 +63,13 @@ struct swaphints_dev {
 #define TYPE(minor)	(((minor) >> 4) & 0xf)	/* high nibble */
 #define NUM(minor)	((minor) & 0xf)		/* low  nibble */
 
-
 /*
  * The different configurable parameters
  */
 extern int swaphints_major;     /* main.c */
 extern int swaphints_nr_devs;
-//extern int swaphints_quantum;
-//extern int swaphints_qset;
 
-//extern int swaphints_p_buffer;	/* pipe.c */
-
-
-/*
- * Prototypes for shared functions
- */
-
-//int     swaphints_p_init(dev_t dev);
-//void    swaphints_p_cleanup(void);
-//int     swaphints_access_init(dev_t dev);
-//void    swaphints_access_cleanup(void);
-
-// int     swaphints_trim(struct swaphints_dev *dev);
-
-// ssize_t swaphints_read(struct file *filp, char __user *buf, size_t count,
-//                    loff_t *f_pos);
-// ssize_t swaphints_write(struct file *filp, const char __user *buf, size_t count,
-//                     loff_t *f_pos);
-// loff_t  swaphints_llseek(struct file *filp, loff_t off, int whence);
-long     swaphints_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
-
+long swaphints_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
 
 /*
  * Ioctl definitions
@@ -159,38 +79,9 @@ long     swaphints_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 #define SWAPHINTS_IOC_MAGIC  'k'
 /* Please use a different 8-bit number in your code */
 
-#define SWAPHINTS_IOCRESET    _IO(SWAPHINTS_IOC_MAGIC, 0)
-
-/*
- * S means "Set" through a ptr,
- * T means "Tell" directly with the argument value
- * G means "Get": reply by setting through a pointer
- * Q means "Query": response is on the return value
- * X means "eXchange": switch G and S atomically
- * H means "sHift": switch T and Q atomically
- */
-#define SWAPHINTS_IOCSQUANTUM _IOW(SWAPHINTS_IOC_MAGIC,  1, int)
-#define SWAPHINTS_IOCSQSET    _IOW(SWAPHINTS_IOC_MAGIC,  2, int)
-#define SWAPHINTS_IOCTQUANTUM _IO(SWAPHINTS_IOC_MAGIC,   3)
-#define SWAPHINTS_IOCTQSET    _IO(SWAPHINTS_IOC_MAGIC,   4)
-#define SWAPHINTS_IOCGQUANTUM _IOR(SWAPHINTS_IOC_MAGIC,  5, int)
-#define SWAPHINTS_IOCGQSET    _IOR(SWAPHINTS_IOC_MAGIC,  6, int)
-#define SWAPHINTS_IOCQQUANTUM _IO(SWAPHINTS_IOC_MAGIC,   7)
-#define SWAPHINTS_IOCQQSET    _IO(SWAPHINTS_IOC_MAGIC,   8)
-#define SWAPHINTS_IOCXQUANTUM _IOWR(SWAPHINTS_IOC_MAGIC, 9, int)
-#define SWAPHINTS_IOCXQSET    _IOWR(SWAPHINTS_IOC_MAGIC,10, int)
-#define SWAPHINTS_IOCHQUANTUM _IO(SWAPHINTS_IOC_MAGIC,  11)
-#define SWAPHINTS_IOCHQSET    _IO(SWAPHINTS_IOC_MAGIC,  12)
-
-/*
- * The other entities only have "Tell" and "Query", because they're
- * not printed in the book, and there's no need to have all six.
- * (The previous stuff was only there to show different ways to do it.
- */
-#define SWAPHINTS_P_IOCTSIZE _IO(SWAPHINTS_IOC_MAGIC,   13)
-#define SWAPHINTS_P_IOCQSIZE _IO(SWAPHINTS_IOC_MAGIC,   14)
-/* ... more to come */
-
-#define SWAPHINTS_IOC_MAXNR 14
+#define SWAPHINTS_IOCRESET     _IO(SWAPHINTS_IOC_MAGIC, 0)
+#define SWAPHINTS_SEND_REQUEST _IOW(SWAPHINTS_IOC_MAGIC,  1, int)
+#define SWAPHINTS_GET_RESPONSE _IOW(SWAPHINTS_IOC_MAGIC,  2, int)
+#define SWAPHINTS_IOC_MAXNR 2
 
 #endif /* _SWAPHINTS_H_ */
